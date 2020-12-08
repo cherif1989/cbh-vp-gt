f1110e713dae52225771326fe012122da8fe0c5f
cherif1989 <61421619+cherif1989@users.noreply.github.com>
2020-12-08 15:28:18 +0100

    Update set-classes.py

--- set-classes.py
+++ set-classes.py
@@ -106,34 +106,39 @@ print('##############################################')
 ######################################################################################################################
 
 
-def check_car(c, dic):
+def check_car(sn, dic):
     # a function to check whether the car is expected to enter in LC
     # dic is the list of cars located in LC
     # the so-called list is stored in the table table_cars_at_lc_0
-    if c[0] in dic:
-        return "car accepted because it belonfs to expected cars"
+    if sn in dic:
+        return "car accepted because it belongs to expected cars"
     else:
         return "car not expected"
 
 car_test_entrance = ['X5', 'V1', 'L1']
-print(check_car(car_test_entrance, table_cars_expected_0))
+print(check_car(car_test_entrance[0], table_cars_expected_0))
 print('test check car #######################""')
 
 
 valid_placements_0 = ['F50', 'F49', 'F48', 'F47', 'F46', 'F45', 'F44', 'F43', 'F42', 'F41', 'F40', 'F39', 'F38', 'F37', 'F36', 'F35','F34','F33','F32','F31','F30','F29','F28','F27','F26','F25','F24','F23','F22','F21','F20','F19','F18','F17','F16','F15','F14','F13','F12','F11','F10','F9','F8','F7','F6','F5','F4','F3','F2','F1']
+table_files_0 = {}
+table_files = {}
+
 table_cars_at_lc_0 = {}
 table_cars_expected_0 = {}
 table_cars_left_lc_0 = {}
-table_files_0 = {}
 
 
-def initialize_files():
-    for elem in valid_placements_0:
-        table_files_0[elem] = File()
-        table_files_0[elem].file_name = elem
+def initialize_files(vp):
+    x = dict()
+    for elem in vp:
+        x[elem] = File()
+        x[elem].file_name = elem
+    return x
 
 
-initialize_files()
+table_files_0 = initialize_files(valid_placements_0)
+table_files = table_files_0
 
 
 def add_car_to_lc(ca: Car, table_cars_at_lc):
@@ -147,8 +152,13 @@ def add_car_to_lc(ca: Car, table_cars_at_lc):
 
 car = Car()
 car.set_car_properties('X1', 'V1', 'L1', "", 0, [])
-
 add_car_to_lc(car, table_cars_at_lc_0)
+
+car2 = Car()
+car2.set_car_properties('X2', 'V2', 'L2', "", 0, [])
+add_car_to_lc(car2, table_cars_at_lc_0)
+
+
 print(table_cars_at_lc_0)
 print("???????????????????????????????adding a car to lc")
 print(car.get_car_properties())
@@ -156,104 +166,41 @@ print(car.get_car_properties())
 
 def assign_car_placement(c: Car, placement):
     if placement in valid_placements_0:
-        c.file = placement
-        c.status = 2
-        c.changelog.append("car " + c.stock_number + " was moved to file " + placement + " at time " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        table_files_0[placement].decrement_file()
+        if table_files[placement].occupied < 5:
+            c.file = placement
+            c.status = 2
+            c.changelog.append("car " + c.stock_number + " was moved to file " + placement + " at time " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            table_files_0[placement].decrement_file()
+        else:
+            return "not possible to add the car in file"
+    else:
+        return "not possible"
 
 
-assign_car_placement(car, "F10")
+print(table_files["F10"])
+print(table_files)
+
 print(car.__dict__)
 print(table_files_0['F10'].__dict__)
 print(table_files_0['F11'].__dict__)
 print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-
-
-
-All_Cars = {
-    'X1': ["X1", "V1", "L1", "E1"],
-    'X2': ["X2", "V2", "L2", "E2"],
-    'X3': ["X3", "V3", "L3", "E3"]
-}
-
-# tomorrow
-
-# csv = [["X4", "V4", "L4", "E4"], ["X5", "V5", "L5", "E5"], ["X1", "V1", "L1", "E1"]]
-
-# for elem in csv:
-#    if elem[0] exists in All_cars:
-#    else:
-#        All_Cars.append(elem)
-
-
-car.set_car_properties("SN123456", "XXXXXXXX", "12_DFG_566", "B1", 0, ["F"])
-print(car.get_car_properties())
-
-print(car.__dict__)
-
-
-# Definition of class File which refers to 'rangée' in French
-# a file usually belongs to a zone, therefore we add it in attributes
-
-operator1 = Operator()
-operator1.set_operator_attributes("john", "wellington", "john.wellignton@auto1.com", "responsible of dispatch")
-
-file_example = File()
-print(file_example.__dict__)
-
-file_example.decrement_file()
-print(file_example.__dict__)
-
-# Simulation: a car enters to the LC, what happens next
-# Set car properties by reading the RD : planned cars in LC
-
-# import pandas
-
-# a new car enters
-
-
-car = Car()
-print(car.__dict__)
-
-# input function : the operator can enrich the new object with attributes
-
-
-# let's say we have files empty
-F1 = File()
-F2 = File()
-F3 = File()
-F4 = File()
-F5 = File()
-F6 = File()
-F7 = File()
-F8 = File()
-F9 = File()
-F10 = File()
+print("===============================================")
 
 car1 = Car()
-
-car1.vin = "CDDKJHZKJEDHZ"
-car1.stock_number = "CC7812Z1"
-
-F1.decrement_file()
-car1.file = "F1"
-
+car1.set_car_properties("xxxx", "vvvv", "llll", "", 0, [])
 print(car1.__dict__)
-print(F1.__dict__)
 
-car3 = Car()
-car3.stock_number = input("hey please enter stock number")
-car3.vin = input("hey please enter vin")
-car3.file = input("hey please choose a valid free place in the file")
+print(table_cars_expected_0)
+table_cars_expected_0["xxxx"] = ["xxxx", "vvvv", "cccc"]
+print(check_car(car1.stock_number, table_cars_expected_0))
 
-# since a place wan assignde to the car, i need to modify the file
-F1.decrement_file()
+add_car_to_lc(car1, table_cars_at_lc_0)
+print(car1.__dict__)
 
-car3.changelog.append(
-    car3.stock_number + " was moved to " + F1.file_name + " at time" + datetime.datetime.now().strftime(
-        "%Y-%m-%d %H:%M:%S") + " " + operator1.firstname + " " + operator1.surname)
+assign_car_placement(car1, "F11")
+print(car1.__dict__)
 
-print(car3.__dict__)
-print(F1.__dict__)
-print(operator1.__dict__)
-car3.display_changelog()
+def car_ready_to_leave_lc(c: Car, table_cars_at_lc):
+    c.status = 3
+    x = car.file
+    c.file = "exit zone"
